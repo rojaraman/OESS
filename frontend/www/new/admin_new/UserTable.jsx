b6bc5f66cced7f8134889faa32436688e7b8cc91
Roja Raman <rojraman@rojraman-dev.grnoc.iu.edu>
2019-11-25 20:58:30 +0000

    Render table data dynamically

---
+++ frontend/www/new/admin_new/UserTable.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+export default class UserTable extends React.Component {
+    
+    constructor(props){
+      super(props);
+      this.getHeader = this.getHeader.bind(this);
+      this.getRowsData = this.getRowsData.bind(this);
+      this.getKeys = this.getKeys.bind(this);
+    }
+    
+    getKeys(){
+      return Object.keys(this.props.data[0]);
+    }
+    
+    getHeader(){
+      var keys = this.getKeys();
+      return keys.map((key, index)=>{
+        return <th key={key}>{key.toUpperCase()}</th>
+      })
+    }
+    
+    getRowsData(){
+      var items = this.props.data;
+      var keys = this.getKeys();
+      //console.log("keys ",keys);
+     // console.log("index",items);
+      return items.map((row, index)=>{
+        return <tr key={index}><RenderRow key={index} data={row} keys={keys}/></tr>
+      })
+    }
+    
+    render() {
+	
+        return (
+          <div>
+            <table>
+            <thead>
+              <tr>{this.getHeader()}</tr>
+            </thead>
+            <tbody>
+	      
+              {this.getRowsData()}
+            </tbody>
+            </table>
+          </div>
+          
+        );
+    }
+}
+
+const RenderRow = (props) =>{
+  return props.keys.map((key, index)=>{
+   console.log("Render Row ", props.data[key]);
+    return <td key={props.data[key]}>{props.data[key]}</td>
+  })
+}
--- frontend/www/new/admin_new/index.jsx
+++ frontend/www/new/admin_new/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import UserTable from './UserTable.jsx';
 
 let path = 'https://rojraman-dev.grnoc.iu.edu/oess/';
 async function getUsers() {
@@ -22,14 +23,14 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-	   users:{
+	   users:[{
                     "email_address":"",
                     "status":"",
                     "type":"",
                     "user_id":"",
                     "family_name":"",
                     "first_name":""
-                }
+                }]
 	};
     }
    
@@ -50,8 +51,29 @@ class App extends React.Component {
      }
       
        render() {
-	console.log(this.state.users);
-        return <h1>Hello</h1>;
+	if(this.state.users[0].user_id != ""){
+		console.log(this.state.users);
+		console.log(this.state.users.length);
+		var users_data = [];
+	
+		var count = 0;
+		this.state.users.forEach(function(obj){
+			var data = {};
+			data["First Name"] = obj.first_name;
+			data["Last Name"] = obj.family_name;
+			data["Username"] = obj.user_id;
+			data["Email Address"] = obj.email_address;
+			data["User Type"] = obj.type;
+			data["User Status"] = obj.status;
+			users_data[count] = data;
+			count = count +1;
+		});
+	
+		console.log(JSON.stringify(users_data));
+		return <UserTable data={users_data}/>;	
+            }else{
+		return null;
+		}
 	}
 }
 
